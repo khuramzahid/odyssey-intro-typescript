561a64dc3d32a060427fd0d838ccb56d6bf5b749
Khuram Zahid <khuramzahid@gmail.com>
2025-04-25 15:24:25 +0500

    Resolver chains

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,4 +1,5 @@
-import { Resolvers } from './types'
+import { Resolvers } from './types';
+import { validateFullAmenities } from "./helpers";
 
 export const resolvers: Resolvers = {
   Query: {
@@ -8,5 +9,15 @@ export const resolvers: Resolvers = {
     listing: (_, { id }, { dataSources }) => {
       return dataSources.listingAPI.getListing(id);
     },
+  },
+  Listing: {
+    amenities: ({ id, amenities }, _, { dataSources }) => {
+      // There are two scenarios we need our Listing.amenities resolver to handle.
+      // If we've queried for a single listing, we'll already have full amenity data available on the parent argument. In this case, we can return the amenities directly.
+      // If our resolver's parent argument comes from Query.featuredListings, however, we'll have only an array of amenity IDs. In this case, we'll need to make a follow-up request to the REST API!
+      return validateFullAmenities(amenities)
+        ? amenities
+        : dataSources.listingAPI.getAmenities(id);
+    }
   },
 };
